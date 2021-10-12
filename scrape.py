2ec15d6c324476209fd11a7485d31906076198a2
luke robertson <robertson.stuff@gmail.com>
2021-10-12 13:28:37 +1100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,8 +4,8 @@ import re
 html = request.urlopen("https://yourwebsite.com")
 text = html.read()
 CleanUp = text.decode('utf8')
-SearchFor = re.findall(r"yourtag/", plaintext)
+SearchFor = re.findall(r"yourtag/", CleanUp)
 
-# SearchFor = re.findall(r"yourtag/\w{3,50}", plaintext)Using Regex, this will search for your tag followed by 3 - 50 alphanumeric characters. 
+# SearchFor = re.findall(r"yourtag/\w{3,50}", CleanUp)Using Regex, this will search for your tag followed by 3 - 50 alphanumeric characters. 
 
 print(SearchFor)
